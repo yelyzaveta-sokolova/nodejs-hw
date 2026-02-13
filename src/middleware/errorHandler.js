48fb1b6cff11c27d950ddcdfbf53e2ab758844f3
yelyzaveta-sokolova <sokolova.yelyzavetaaa@gmail.com>
2026-02-13 13:47:20 +0100

    fix: proper HttpError handling in errorHandler

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,7 +1,13 @@
-export const errorHandler = (err, req, res, next) => {
-  const status = err.status || 500;
+import createHttpError from 'http-errors';
 
-  res.status(status).json({
-    message: err.message || 'Internal Server Error',
+export const errorHandler = (err, req, res, next) => {
+  if (err instanceof createHttpError.HttpError) {
+    return res.status(err.status).json({
+      message: err.message,
+    });
+  }
+
+  return res.status(500).json({
+    message: 'Internal Server Error',
   });
 };
